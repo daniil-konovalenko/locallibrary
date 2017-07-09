b57302d5f41054426fa15ddc5104175dae093a02
Daniel Konovalenko <ddkono@gmail.com>
2017-07-09 23:19:39 +0300

    Add routers stubs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var mongoose = require('mongoose');
 
@@ -47,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
